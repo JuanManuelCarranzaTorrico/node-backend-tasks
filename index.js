d8a9857c6d3817f4063031a7efe8a07bbf4358a2
JuanManuelCarranzaTorrico <73134507+JuanManuelCarranzaTorrico@users.noreply.github.com>
2021-08-22 21:22:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.get("/", (req, res, next) => {
 
 app.post("/tasks", jsonParser, (req, res, next) => {
     req.body.id = tasks.length + 1;
+    req.body.status = "pendiente";
     tasks.push(req.body);
+    
     res.send("OK");
 });
 
@@ -24,6 +26,36 @@ app.get("/tasks", (req, res, next) => {
     res.json(tasks);
 });
 
+app.get("/tasks/:taskId", (req, res) => {
+    id=req.params.taskId-1;
+    res.send(tasks[id]);
+});
+
 app.listen(3000, () => {
     console.log("Servidor HTTP funcionando");
+});
+
+app.delete("/tasks/:taskId", (req, res, next) => {
+    id=req.params.taskId-1;
+    tasks.splice(id, 1);
+    res.send("Deleted");
+});
+
+
+app.put("/tasks/:taskId", jsonParser, (req, res, next) => {
+    var id = req.params.taskId-1;
+    var state = req.query.state;
+    if(state==="edit"){
+    tasks[id].title=req.body.title;
+    tasks[id].detail=req.body.detail;
+    res.send("Updated");
+    }
+    if(state==="completado"){
+        tasks[id].status="completado";
+        res.send("Updated");
+        }
+    if(state==="pendiente"){
+        tasks[id].status="pendiente";
+        res.send("Updated");
+        }
 });
